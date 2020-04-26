b58fb8b2072d615120975de3566f8be41b6f350a
eera-l <federica.comuni@gmail.com>
2020-04-26 11:21:30 +0200

    final problem2

--- assignment3/problem2a.py
+++ assignment3/problem2a.py
@@ -2,7 +2,6 @@ import numpy as np
 import math
 from pyspark import SparkContext
 import time
-import matplotlib.pyplot as plt
 
 
 def start_process(workers, file_name):
@@ -33,16 +32,29 @@ def make_histogram(values, bin_num):
     return bins, min_value, max_value, step
 
 
+def calculate_median(values, length):
+    values = values.sortBy(lambda x: x).collect()
+    if length % 2 == 0:
+        median = (values[int((length / 2) - 1)] + values[int(length / 2)]) / 2
+    else:
+        median = values[int(length / 2)]
+    return median
+
+
 def run_workers(workers):
     start = time.time()
     val, length, sc = start_process(workers, '/data/2020-DAT346-DIT873-TLSD/DATASETS/assignment3.dat')
+    print("{0:19s} {1:-8d}".format("Count", length))
     mean = calculate_mean(val, length)
     print("{0:20s} {1:-3.5f}".format("Mean", mean))
     std = calculate_std(val, mean, length)
     print("{0:20s} {1:-3.5f}".format("Standard deviation", std))
+    median = calculate_median(val, length)
+    print("{0:20s} {1:-3.5f}".format("Median", median))
     bins, mini, maxi, step = make_histogram(val, 10)
     print("{0:20s} {1:-3.5f}".format("Min value", mini))
     print("{0:20s} {1:-3.5f}".format("Max value", maxi))
+    print("---------------------------------")
     print("{0:11s} {1:10s}".format("Bin range", "Occurrences"))
     for tup in bins:
         print("{0:.2f} - {1:.2f} {2:-10d}".format(mini + tup[0] * step, (mini + tup[0] * step)
@@ -54,35 +66,5 @@ def run_workers(workers):
     return duration
 
 
-def main_multiprocessing():
-    cores = [1, 2, 4, 8, 16, 32]
-    durations =[run_workers(core) for core in cores]
-    theoretical_durations = [(durations[0] / core) for core in cores]
-    speedups = [(durations[0] / duration) for duration in durations]
-
-    fig, axes = plt.subplots(1, 2)
-    fig.set_size_inches(12, 6)
-    fig.suptitle(f"Statistics computation performance with PySpark")
-
-    axes[0].grid()
-    axes[0].set_xscale("log", basex=2)
-    axes[0].set_xlabel("Cores")
-    axes[0].set_ylabel("Speedup")
-    axes[0].plot(cores, cores, "ro-", label="Theoretical speedup")
-    axes[0].plot(cores, speedups, "bo-", label="Actual speedup")
-    axes[0].legend()
-
-    axes[1].grid()
-    axes[1].set_xscale("log", basex=2)
-    axes[1].set_xlabel("Cores")
-    axes[1].set_ylabel("Time taken (s)")
-    axes[1].plot(cores, theoretical_durations, "ro-", label="Theoretical time")
-    axes[1].plot(cores, durations, "bo-", label="Actual time")
-    axes[1].legend()
-
-    plt.savefig("problem2a.png")
-    # plt.show()
-
-
 if __name__ == "__main__":
-    main_multiprocessing()
+    run_workers(4)
