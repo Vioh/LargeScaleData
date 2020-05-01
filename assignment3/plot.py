82feaf0ee50fa74e85749eb15ff12ced4419426a
Hai Dinh <dinhsyhai@gmail.com>
2020-05-01 21:04:44 +0200

    Adapt plot.py and run.py to use in all subproblems

--- assignment3/plot.py
+++ assignment3/plot.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 
-def compute(num_cores):
-    script_path = os.path.dirname(os.path.abspath(__file__)) + "/problem1.py"
+def compute(script, num_cores):
+    script_path = os.path.dirname(os.path.abspath(__file__)) + "/" + script
     script_args = ["--local-tmp-dir", "output", "--runner", "local", "--num-cores", str(num_cores)] + sys.argv[1:]
     command = ["python3", script_path] + script_args
 
@@ -22,7 +22,7 @@ def compute(num_cores):
 
 def main(args):
     cores = [1, 2, 4, 8, 16, 32]
-    durations = [compute(core) for core in cores]
+    durations = [compute(args.script, core) for core in cores]
     theoretical_durations = [(durations[0] / core) for core in cores]
     speedups = [(durations[0] / duration) for duration in durations]
 
@@ -65,7 +65,14 @@ if __name__ == "__main__":
     parser.add_argument("--output", "-o",
                         default=None,
                         type=str,
-                        help="Name of the file to output the plot (optional)")
+                        help="Name of the file to output the plot")
+
+    required_args = parser.add_argument_group("required arguments")
+    required_args.add_argument("--script", "-s",
+                                required=True,
+                                type=str,
+                                help="Name of the script to use for plotting")
+
     known_args, other_args = parser.parse_known_args()
     sys.argv = sys.argv[:1] + other_args
     main(known_args)
--- assignment3/run.sh
+++ assignment3/run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if [[ $# == 1 ]]; then
-    label="${1}_"
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "Usage: ${0} script_name [label]"
+    exit 0
+fi
+
+if [[ $# == 2 ]]; then
+    label="${2}_"
 fi
 
 now=$(date +"%y%m%d_%H%M%S")
-datafile=/data/2020-DAT346-DIT873-TLSD/DATASETS/assignment3.dat
+data_file=/data/2020-DAT346-DIT873-TLSD/DATASETS/assignment3.dat
+script_name=${1}
 
 export SLURM_JOB_CPUS_PER_NODE=32
 sbatch --cpus-per-task=32 --output output/${label}${now}.out /opt/local/bin/run_job.sh \
-        plot_problem1.py --output output/${label}${now}.png ${datafile}
+        plot.py -o output/${label}${now}.png -s ${script_name} ${data_file}
