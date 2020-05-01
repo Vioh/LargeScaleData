c16fa1c7fc251a878e706f397b99537eb03fab27
Hai Dinh <dinhsyhai@gmail.com>
2020-05-01 16:10:29 +0200

    Remove usage of keys in combiners and reducers
    The usage of two keys (stats1 and stats2) gives us better speedups
    in practice, but actually takes more time to compute if we compare
    to when we use no keys at all, regardless of numbers of cores.
    
    For combiners, using 2 separate keys doesn't even make sense, since
    mrjob already parallelize by splitting the data into different cores
    anyway. Using the keys here can even bring the overhead of having to
    write more data into the disk, since the mappers would have to yield
    2 keys for each of the lines.
    
    For reducers, using 2 keys can have a speedup of maximum 2. However,
    the workload for the first key (stats1) is too small, so it is not
    worth it to use the keys here either.
    
    Generally, it is much better to parallelize the data instead of the
    keys. An added benefit is that the code looks much cleaner now.

--- assignment3/problem1.py
+++ assignment3/problem1.py
@@ -3,10 +3,6 @@ import math
 import heapq
 from mrjob.job import MRJob
 
-# Keys:
-#   stats1 will be used as key for [mean, stdev]
-#   stats2 will be used as key for [min, max, median, bins]
-
 class ComputeStatistics(MRJob):
 
     def configure_args(self):
@@ -20,72 +16,36 @@ class ComputeStatistics(MRJob):
     def mapper(self, _, line):
         _, group, value = line.split("\t")
         filter_group = self.options.group
-        value = float(value)
 
         if filter_group is None or int(group) == filter_group:
-            yield ("stats1", value)
-            yield ("stats2", value)
+            yield (None, float(value))
 
 
-    def combiner(self, key, values):
-        if key == "stats1":
-            yield ("stats1", self.combiner_stats1(values))
-        elif key == "stats2":
-            yield ("stats2", self.combiner_stats2(values))
-
-
-    def reducer(self, key, data):
-        if key == "stats1":
-            mean_value, stdev, n_values = self.reducer_stats1(data)
-
-            yield ("Count", n_values)
-            yield ("Mean", mean_value)
-            yield ("Stdev", stdev)
-
-        elif key == "stats2":
-            min_value, max_value, median, bins = self.reducer_stats2(data)
-
-            yield("Min", min_value)
-            yield("Max", max_value)
-            yield("Median", median)
-
-            for idx, bin_count in enumerate(bins):
-                yield("Bin{}".format(idx+1), bin_count)
-
-
-    def combiner_stats1(self, values):
-        sum_of_values, sum_of_squares, n_values = 0, 0, 0
+    def combiner(self, _, values):
+        values = sorted(values)
+        sum_of_values, sum_of_squares = 0, 0
 
         for v in values:
             sum_of_values += v
             sum_of_squares += v**2
-            n_values += 1
 
-        return sum_of_values, sum_of_squares, n_values
-
-
-    def combiner_stats2(self, values):
-        return sorted(values)
+        yield (None, (sum_of_values, sum_of_squares, values))
 
 
-    def reducer_stats1(self, tuples):
-        sum_of_values, sum_of_squares, n_values = 0, 0, 0
+    def reducer(self, _, tuples):
+        sum_of_values, sum_of_squares, sorted_lists = 0, 0, []
 
         for t in tuples:
             sum_of_values += t[0]
             sum_of_squares += t[1]
-            n_values += t[2]
+            sorted_lists.append(t[2])
 
-        mean_value = sum_of_values / n_values
-        stdev = math.sqrt(sum_of_squares / n_values - mean_value**2)
-        return mean_value, stdev, n_values
-
-
-    def reducer_stats2(self, sorted_lists):
         values = list(heapq.merge(*sorted_lists))
         n_values = len(values)
         min_value = values[0]
         max_value = values[-1]
+        mean_value = sum_of_values / n_values
+        stdev = math.sqrt(sum_of_squares / n_values - mean_value**2)
 
         if n_values % 2 == 0:
             median = (values[int((n_values/2) - 1)] + values[int(n_values/2)]) / 2
@@ -98,7 +58,15 @@ class ComputeStatistics(MRJob):
             bin_idx = -1 if v == max_value else int((v - min_value) / step)
             bins[bin_idx] += 1
 
-        return min_value, max_value, median, bins
+        yield("Count", n_values)
+        yield("Mean", mean_value)
+        yield("Stdev", stdev)
+        yield("Min", min_value)
+        yield("Max", max_value)
+        yield("Median", median)
+
+        for idx, bin_count in enumerate(bins):
+            yield("Bin{}".format(idx+1), bin_count)
 
 
 if __name__ == "__main__":
